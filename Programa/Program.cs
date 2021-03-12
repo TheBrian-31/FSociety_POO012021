76f147b53323a7c7fa1b3f9518a1f16a0b33a3e4
Bryan31542 <00063820@uca.edu.sv>
2021-03-12 15:15:02 -0600

    Pushing AddNumbers

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -6,7 +6,21 @@ namespace Programa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0, b = 0;
+
+            Console.Write("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a,b)}");
+        
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
         }
     }
 }
